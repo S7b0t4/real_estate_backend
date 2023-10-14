1202bcfdd433f2e26eb007b79a7d3ea623976abc
S7b0t4 <s7b0t4@MacBook-Pro-Maksim-2.local>
2023-10-14 23:55:53 +0500

    commit

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import fs from 'fs';
 
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import middlewares from "./middlewares"
 
 const corsOrigin ={
     origin:'http://localhost:3000',
@@ -55,6 +56,7 @@ const srcStorage = multer.diskStorage({
 
 app.use('/src', Express.static(srcDirectory));
 
+
 async function startApp() {
 	console.log("server is starting")
 	try{
@@ -86,31 +88,28 @@ app.get("/", async (req, res) => {
 	res.json(Posts)
 })
 
+const getRandomElementsFromArray = (arr, numElements) => {
+	numElements = Math.min(numElements, arr.length);
+	const copyArray = [...arr];
+	const randomElements = [];
+	for (let i = 0; i < numElements; i++) {
+		const randomIndex = Math.floor(Math.random() * copyArray.length);
+		randomElements.push(copyArray.splice(randomIndex, 1)[0]);
+	}
+	return randomElements;
+}
+
 app.get("/random", async (req, res) => {
 	const Posts = await Post.find();
-	console.log("getRandom")
 	res.set("Access-Control-Allow-Origin", "*")
-	getRandomElementsFromArray = (arr, numElements) => {
-		numElements = Math.min(numElements, arr.length);
-		const copyArray = [...arr];
-		const randomElements = [];
-		for (let i = 0; i < numElements; i++) {
-			const randomIndex = Math.floor(Math.random() * copyArray.length);
-			randomElements.push(copyArray.splice(randomIndex, 1)[0]);
-		}
-		return randomElements;
-	}
-	
 	const randomElements = getRandomElementsFromArray(Posts, 4);
 	res.json(randomElements)
 })
 
 app.get('/:id', async (req, res) => {
-	console.log("getById")
 	res.set("Access-Control-Allow-Origin", "*")
 	const Posts = await Post.find();
 	const id = req.params.id;
-	console.log(id)
 	const obj = Posts.find(obj => obj._id.toString() === id)
 	res.send(obj)
  });
