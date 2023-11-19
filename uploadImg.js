a786a438e745361002245233f3e1982593fe3dd8
S7b0t4 <s7b0t4@Mac-OS.local>
2023-11-19 18:17:02 +0500

    add uploadImg

---
+++ uploadImg.js
@@ -0,0 +1,36 @@
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+import path from 'path'
+import multer from 'multer'
+
+const uploadImg = () => {
+
+	const __filename = fileURLToPath(import.meta.url)
+	const __dirname = dirname(__filename)
+
+	const uploadDirectory = path.join(__dirname, 'uploads')
+
+	const storage = multer.diskStorage({
+		destination: function (req, file, cb) {
+			cb(null, uploadDirectory)
+		},
+		filename: function (req, file, cb) {
+			cb(null, file.originalname)
+		}
+	})
+
+	const upload = multer({ storage: storage })
+	app.use('/uploads', Express.static(uploadDirectory))
+
+	const srcDirectory = path.join(__dirname, 'src')
+
+	app.use('/src', Express.static(srcDirectory)) 
+
+	app.post("/post-photo", upload.single('file'), async (req, res) => {
+		res.set("Access-Control-Allow-Origin", "*")
+		console.log(req.body)
+		res.status(200).json("Successful")
+	})
+}
+export default uploadImg
