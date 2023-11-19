ff117362dc32a4529b1c66bb1e42e19b877d06fa
S7b0t4 <s7b0t4@Mac-OS.local>
2023-11-19 17:49:44 +0500

    h

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-import Express from 'express';
-import mongoose from 'mongoose';
-import Post from './Post.js';
+import Express from 'express'
+import mongoose from 'mongoose'
+import Post from './Post.js'
 import multer from 'multer'
-import cors from 'cors';
-import path from 'path';
-import fs from 'fs';
-import fetch from 'node-fetch';
+import cors from 'cors'
+import path from 'path'
+import fs from 'fs'
+import fetch from 'node-fetch'
 
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
-const corsOrigin ={
-    origin:'http://localhost:3000',
-    credentials:true,            
-    optionSuccessStatus:200
+const corsOrigin = {
+	origin: 'http://localhost:3000',
+	credentials: true,
+	optionSuccessStatus: 200
 }
 
 const DB_URL = "mongodb+srv://S7b0t4:228008@cluster0.cpkfqq3.mongodb.net/?retryWrites=true&w=majority"
@@ -22,107 +22,98 @@ const PORT = 5000
 
 const app = Express()
 
-app.use(cors(corsOrigin));
-app.use(Express.json());
+app.use(cors(corsOrigin))
+app.use(Express.json())
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-const uploadDirectory = path.join(__dirname, 'uploads');
+const uploadDirectory = path.join(__dirname, 'uploads')
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDirectory);
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
+	destination: function (req, file, cb) {
+		cb(null, uploadDirectory)
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname)
+	}
+})
 
-const upload = multer({ storage: storage });
-app.use('/uploads', Express.static(uploadDirectory));
+const upload = multer({ storage: storage })
+app.use('/uploads', Express.static(uploadDirectory))
 
-const srcDirectory = path.join(__dirname, 'src');
+const srcDirectory = path.join(__dirname, 'src')
 
-const srcStorage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, srcDirectory);
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
-
-app.use('/src', Express.static(srcDirectory));
+app.use('/src', Express.static(srcDirectory))
 
 
 async function startApp() {
 	console.log("server is starting")
-	try{
+	try {
 		await mongoose.connect(DB_URL, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true
-	})
-		app.listen(PORT, ()=>console.log("Server has been started on", PORT ))
+		})
+		app.listen(PORT, () => console.log("Server has been started on", PORT))
 	}
-	catch(e){
+	catch (e) {
 		console.log(e)
 	}
 }
 const addToDB = async (req, res) => {
 	console.log(req.body)
-	try{
-		const { title, subTitle, sell, rent, link, img, squareImg, mainIMG, filterTag, compInfo, cost, infoNumber, iconMapIMG, tag, textInfo, linkInfo,} = req.body
-		const post = await Post.create({title, subTitle, sell, rent, link, img, squareImg, mainIMG, filterTag, compInfo, cost, infoNumber, iconMapIMG, tag, textInfo, linkInfo}) 
+	try {
+		const { title, subTitle, sell, rent, link, img, squareImg, mainIMG, filterTag, compInfo, cost, infoNumber, iconMapIMG, tag, textInfo, linkInfo, } = req.body
+		const post = await Post.create({ title, subTitle, sell, rent, link, img, squareImg, mainIMG, filterTag, compInfo, cost, infoNumber, iconMapIMG, tag, textInfo, linkInfo })
 		res.status(200).json(post)
-	}catch(e){
+	} catch (e) {
 		console.log(e)
 	}
 }
 startApp()
 
 app.get("/", async (req, res) => {
-	const Posts = await Post.find();
+	const Posts = await Post.find()
 	res.set("Access-Control-Allow-Origin", "*")
 	res.json(Posts)
 })
 
 const getRandomElementsFromArray = (arr, numElements) => {
-	numElements = Math.min(numElements, arr.length);
-	const copyArray = [...arr];
-	const randomElements = [];
+	numElements = Math.min(numElements, arr.length)
+	const copyArray = [...arr]
+	const randomElements = []
 	for (let i = 0; i < numElements; i++) {
-		const randomIndex = Math.floor(Math.random() * copyArray.length);
-		randomElements.push(copyArray.splice(randomIndex, 1)[0]);
+		const randomIndex = Math.floor(Math.random() * copyArray.length)
+		randomElements.push(copyArray.splice(randomIndex, 1)[0])
 	}
-	return randomElements;
+	return randomElements
 }
 
 app.get("/random", async (req, res) => {
-	const Posts = await Post.find();
+	const Posts = await Post.find()
 	res.set("Access-Control-Allow-Origin", "*")
-	const randomElements = getRandomElementsFromArray(Posts, 4);
+	const randomElements = getRandomElementsFromArray(Posts, 4)
 	res.json(randomElements)
 })
 
 app.get("/get-data", async (req, res) => {
 	res.set("Access-Control-Allow-Origin", "*")
-  fs.readdir(uploadDirectory, (err, files) => {
-    if (err) {
-      return res.status(500).send('Ошибка чтения папки загрузок');
-    }
-    res.send(files)
-	});
-});
+	fs.readdir(uploadDirectory, (err, files) => {
+		if (err) {
+			return res.status(500).send('Ошибка чтения папки загрузок')
+		}
+		res.send(files)
+	})
+})
 
 app.post("/delete", (req, res) => {
-	try{
-		Post.deleteMany({ minimum_nights:"2" }).then((result) => {
-    console.log(result);
-		res.status(200).json("Successful")
-	});
-	}catch(e){
+	try {
+		Post.deleteMany({ minimum_nights: "2" }).then((result) => {
+			console.log(result)
+			res.status(200).json("Successful")
+		})
+	} catch (e) {
 		res.status(500).json(e)
 	}
 })
@@ -146,23 +137,23 @@ let usdtCoin = 0
 
 const getCoin = (i, link, key) => {
 	fetch(link, {
-	headers: {
-	    "X-CoinAPI-Key": key
-	    }
-	})
-	.then(response => response.json())
-	.then(data => {
-	  if(i === "ethCoin"){
-			ethCoin = "~$"+data.rate.toFixed(2)
-		}
-		if(i === "btcCoin"){
-			btcCoin = "~$"+data.rate.toFixed(2)
-		}
-		if(i === "usdtCoin"){
-			usdtCoin = "~$"+data.rate.toFixed(9)
+		headers: {
+			"X-CoinAPI-Key": key
 		}
 	})
-	.catch(error => console.error('Error:', error));
+		.then(response => response.json())
+		.then(data => {
+			if (i === "ethCoin") {
+				ethCoin = "~$" + data.rate.toFixed(2)
+			}
+			if (i === "btcCoin") {
+				btcCoin = "~$" + data.rate.toFixed(2)
+			}
+			if (i === "usdtCoin") {
+				usdtCoin = "~$" + data.rate.toFixed(9)
+			}
+		})
+		.catch(error => console.error('Error:', error))
 }
 
 getCoin("ethCoin", 'https://rest.coinapi.io/v1/exchangerate/ETH/USD', "8BCFCBCB-2BDA-4F41-9B8A-D80C5020178F")
@@ -171,7 +162,7 @@ getCoin("usdtCoin", 'https://rest.coinapi.io/v1/exchangerate/USDT/USD', "5D1FBF8
 
 
 
- app.get("/coins", async (req, res) => {
+app.get("/coins", async (req, res) => {
 	res.set("Access-Control-Allow-Origin", "*")
 	res.send([
 		{
@@ -191,8 +182,8 @@ getCoin("usdtCoin", 'https://rest.coinapi.io/v1/exchangerate/USDT/USD', "5D1FBF8
 
 app.get('/:id', async (req, res) => {
 	res.set("Access-Control-Allow-Origin", "*")
-	const Posts = await Post.find();
-	const id = req.params.id;
+	const Posts = await Post.find()
+	const id = req.params.id
 	const obj = Posts.find(obj => obj._id.toString() === id)
 	res.send(obj)
- });
+})
